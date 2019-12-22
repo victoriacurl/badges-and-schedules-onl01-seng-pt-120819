2e5ba21e2d27d63165dc06a228999a0cb4c10f0b
Victoria Curl <victoriacurl9@gmail.com>
2019-12-22 14:01:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,3 +14,11 @@ end
 def batch_badge_creator(attendees)
   attendees.map { |badges| badge_maker(badges) }
 end
+
+def assign_rooms(name)
+  rooms = []
+
+  name.each.with_index(1) do |name, room_assignment|
+    rooms << room_assignments = "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
+  end
+  rooms
