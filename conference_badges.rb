d522a07f7a1b84e18c40b4d07c2dffcdae2e84c2
Victoria Curl <victoriacurl9@gmail.com>
2019-12-22 13:49:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+#def batch_badge_creator(attendees)
+  #attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+    #badges.each do | badges |
+    #puts "Hello, my name is #{badges}."
+  #end
+#end
